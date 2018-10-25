1d84bffeca8a09c75b98072158d7318f7f681632
Demyan <demyan1411@gmail.com>
2018-10-25 23:13:39 +0300

    Try terraform

---
+++ terraform/files/deploy.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+# set -e
+
+# APP_DIR=${1:-$HOME}
+#
+# git clone -b monolith https://github.com/express42/reddit.git $APP_DIR/reddit
+# cd $APP_DIR/reddit
+# bundle install
+
+echo "===================================================================="
+echo "===================================================================="
+echo "===================================================================="
+echo "===================================================================="
+
+cd /home/appuser
+git clone -b monolith https://github.com/express42/reddit.git /home/appuser/reddit
+cd /home/appuser/reddit
+bundle install
+
+sudo mv /tmp/puma.service /etc/systemd/system/puma.service
+sudo systemctl start puma
+sudo systemctl enable puma
